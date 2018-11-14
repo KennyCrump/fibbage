7682a8a357a15714c5cad50c0d43193558f197ec
Kenny Crump <kennycrump@Kennys-MacBook-Pro.local>
2018-11-14 09:53:35 -0700

    socket testing

--- server/server.js
+++ server/server.js
@@ -1,15 +1,28 @@
-require('dotenv').config()
-const express = require('express')
-const massive = require('massive')
-const session = require('express-session')
-const axios = require('axios')
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const socket = require("socket.io");
+const massive = require("massive");
+// const session = require('express-session')
+const axios = require("axios");
+const app = express();
+const { SERVER_PORT } = process.env;
 
-app.use(express.json())
+app.use(express.json());
 
-massive(CONNECTION_STRING).then((db) => {
-    app.set('db', db)
-    app.listen(SERVER_PORT, () => {
-      console.log(`server running on port ${SERVER_PORT}`)
-    })
-  })
+// massive(CONNECTION_STRING).then((db) => {
+//     app.set('db', db)
+// })
+const server = app.listen(SERVER_PORT, () => {
+  console.log(`server running on port ${SERVER_PORT}`);
+});
+
+const io = socket(server);
+
+io.on("connection", client => {
+  client.on("subscribeToTimer", interval => {
+    console.log("client is subscribed with interval", interval, client.id);
+    setInterval(() => {
+      client.emit("timer", new Date())
+    }, interval);
+  });
+});
